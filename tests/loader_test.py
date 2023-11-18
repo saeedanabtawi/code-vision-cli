7ee55df4243af5b23646309518d958dab7e9d158
Saeed Anabtawi <saeedanabtawi@users.noreply.github.com>
2023-11-18 21:23:36 +0200

    update unit tests

--- tests/loader_test.py
+++ tests/loader_test.py
@@ -1,10 +1,32 @@
 import unittest
 from core.loader import load, filter, helper
 
+class MockCode:
+    def __init__(self, text):
+        self.text = text
 
 class MyTestCase(unittest.TestCase):
     def test_remove_spaces_normal_string(self):
-        self.assertEqual(filter.remove_spaces("Hello World") == "HelloWorld")
+        code = MockCode("Hello World")
+        self.assertEqual(filter.remove_spaces(code), "HelloWorld")
+
+    def test_remove_spaces_no_spaces(self):
+        code = MockCode("HelloWorld")
+        self.assertEqual(filter.remove_spaces(code), "HelloWorld")
+
+    def test_remove_spaces_only_spaces(self):
+        code = MockCode("     ")
+        self.assertEqual(filter.remove_spaces(code), "")
+
+    def test_remove_spaces_special_characters(self):
+        code = MockCode("Hello @ World!")
+        self.assertEqual(filter.remove_spaces(code), "Hello@World!")
+
+    def test_remove_spaces_empty_string(self):
+        code = MockCode("")
+        self.assertEqual(filter.remove_spaces(code), "")
+
+
 
 if __name__ == '__main__':
     unittest.main()
