6da3cea2b041af38302300c1532d9e7ce917f045
Saeed Anabtawi <saeedanabtawi@users.noreply.github.com>
2023-11-22 18:23:51 +0200

    improve lcs performance

--- code_vision_cli/similarity/lcs.py
+++ code_vision_cli/similarity/lcs.py
@@ -1,33 +1,44 @@
+from bisect import bisect_right, bisect_left
+
 # Dynamic programming (longest common sequence)
-def dlcs(a, b):
-    lengths = [[0 for j in range(len(b) + 1)] for i in range(len(a) + 1)]
-    # row 0 and column 0 are initialized to 0 already
-    for i, x in enumerate(a):
-        for j, y in enumerate(b):
+def dlcs(text1, text2):
+    if len(text1) < len(text2):
+        temp = text1
+        text1 = text2
+        text2 = temp
 
-            if x == y:
-                lengths[i + 1][j + 1] = lengths[i][j] + 1
-            else:
-                lengths[i + 1][j + 1] = max(lengths[i + 1][j], lengths[i][j + 1])
-
-    result = ""
-    x, y = len(a), len(b)
-
-    while x != 0 and y != 0:
-        if lengths[x][y] == lengths[x - 1][y]:
-            x -= 1
-        elif lengths[x][y] == lengths[x][y - 1]:
-            y -= 1
+    indices = {}
+    for i in range(len(text1)):
+        c = text1[i]
+        if c not in indices:
+            indices[c] = [i]
         else:
-            assert a[x - 1] == b[y - 1]
-            result = a[x - 1] + result
-            x -= 1
-            y -= 1
-    return result
+            indices[c].append(i)
 
+    seq = []
+    for c in text2:
+        if c not in indices:
+            continue
+        # Find smallest index > seq[-1].
+        # If it exists, add to seq.
+        if not seq:
+            seq.append(indices[c][0])
+        else:
+            idx = bisect_right(indices[c], seq[-1])
+            if idx < len(indices[c]) and indices[c][idx] > seq[-1]:
+                seq.append(indices[c][idx])
+
+            # For indices < seq[-1], replace in seq.
+            for i in range(idx - 1, -1, -1):
+                seq[bisect_left(seq, indices[c][i])] = indices[c][i]
+
+        if not indices[c]:
+            indices.pop(c)
+
+    return len(seq)
 
 def calculate_sim(text_a, text_b):
     try:
-        return (2 * len(dlcs(text_a, text_b))) / (len(text_a) + len(text_b))
+        return (2 * dlcs(text_a, text_b)) / (len(text_a) + len(text_b))
     except Exception as e:
         print('[-] Error : ' + str(e))
